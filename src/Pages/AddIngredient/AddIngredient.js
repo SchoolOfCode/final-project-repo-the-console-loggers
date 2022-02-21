b898f6a6eeab5914b0d9ffd81c4b4ca271ca5e80
Victoriam97 <victoriameah@hotmail.com>
2022-02-21 15:32:48 +0000

    Components/pages folders made

---
+++ src/Pages/AddIngredient/AddIngredient.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddIngredient() {
+  return (
+    <div>AddIngredient</div>
+  )
+}
+
+export default AddIngredient
---
+++ src/Pages/UserSettings/UserSettings.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserSettings() {
+  return (
+    <div>Settings</div>
+  )
+}
+
+export default UserSettings
