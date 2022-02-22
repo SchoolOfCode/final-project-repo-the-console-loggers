88debe32a5b790501503a5c6dbeb01a5ec1df094
Sevi <correosevi@gmail.com>
2022-02-22 20:40:42 +0000

    Home Screen in progress

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Home() {
+  const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
   return (
-    <div className='header'>
-      <h3>Welcome Name</h3>
-      <p>What ingredients shall we use today?</p>
-    </div>
-  )
+    <>
+      <header className="header">
+        <div className="avatar">
+          {/* <img src={user.picture} alt="Avatar" /> */}
+        </div>
+        <div className="welcome-msg">
+          <p className="welcome-name">
+            {/* Welcome <span>{user.name}</span> */}
+          </p>
+          <p className="catch-phrase">What ingredients shall we use today?</p>
+        </div>
+      </header>
+      <main className="main-home">
+        <button className="login-button" onClick={() => loginWithRedirect()}>
+          Login
+        </button>
+        <h3>Welcome Name</h3>
+        <p>What ingredients shall we use today?</p>
+      </main>
+      <nav className="nav-menu">
+        Home - Ingredients- Shopping list - Settings
+      </nav>
+    </>
+  );
 }
 
-export default Home
+export default Home;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,14 @@
 //Component
-import Login from "../../Pages/Login/Login";
-import Nav from "../Nav/Nav";
-import Home from "../../Pages/Home/Home";
-import { useAuth0 } from "@auth0/auth0-react";
-import Router from "../../Router/Router";
+import Login from '../../Pages/Login/Login';
+import Nav from '../Nav/Nav';
+import Home from '../../Pages/Home/Home';
+import { useAuth0 } from '@auth0/auth0-react';
+import Router from '../../Router/Router';
 
 function App() {
   // const { isAuthenticated, isLoading } = useAuth0();
   return (
-    <div>
-      {/* <Nav /> */}
-      <Home />
+    <div className="app">
       <Router />
     </div>
   );
