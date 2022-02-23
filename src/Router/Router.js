6f6eac01d9152196a37b0f4c8f56a1162312c3f1
Sevi <correosevi@gmail.com>
2022-02-23 10:35:24 +0000

    Added login screen
    Co-authored-by: Victoriam97 <victoriameah@hotmail.com>
    Co-authored-by: JEveriss <jacques.everiss@gmail.com>

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "../Pages/Home/Home";
-import RecipeList from "../Pages/RecipeList/RecipeList";
-import ShoppingList from "../Pages/ShoppingList/ShoppingList";
-import UserSettings from "../Pages/UserSettings/UserSettings";
+import { Routes, Route } from 'react-router-dom';
+import Home from '../Pages/Home/Home';
+import RecipeList from '../Pages/RecipeList/RecipeList';
+import ShoppingList from '../Pages/ShoppingList/ShoppingList';
+import UserSettings from '../Pages/UserSettings/UserSettings';
+import Login from '../Pages/Login/Login';
 
 function Router() {
   return (
     <Routes>
       <Route path="home" element={<Home />} />
+      <Route path="login" element={<Login />} />
       <Route path="RecipeList" element={<RecipeList />} />
       <Route path="ShoppingList" element={<ShoppingList />} />
       <Route path="UserSettings" element={<UserSettings />} />
