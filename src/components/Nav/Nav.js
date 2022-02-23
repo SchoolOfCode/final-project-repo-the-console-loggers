93bffa776f8ca440740fb7527f68a1bfced9c554
Sevi <correosevi@gmail.com>
2022-02-23 19:57:16 +0000

    Update icons colors based on high-fi design

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,44 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <div className="nav-wrapper">
       <ul>
         <li>
-          <Link to="Home">
+          <Link className="link" to="Home">
             <img
               className="icon"
-              src={process.env.PUBLIC_URL + "/assets/icons/fridge.svg"}
+              src={process.env.PUBLIC_URL + '/assets/icons/fridge.svg'}
               alt="Home"
             />
             Home
           </Link>
         </li>
         <li>
-          <Link to="RecipeList">
+          <Link className="link" to="RecipeList">
             <img
               className="icon"
-              src={process.env.PUBLIC_URL + "/assets/icons/recipes.svg"}
+              src={process.env.PUBLIC_URL + '/assets/icons/recipes.svg'}
               alt="Recipe List"
             />
             Recipes
           </Link>
         </li>
         <li>
-          <Link to="ShoppingList">
+          <Link className="link" to="ShoppingList">
             <img
               className="icon"
-              src={process.env.PUBLIC_URL + "/assets/icons/shopping-list.svg"}
+              src={process.env.PUBLIC_URL + '/assets/icons/shopping-list.svg'}
               alt="Shopping List"
             />
             <p>Shopping List</p>
           </Link>
         </li>
         <li>
-          <Link to="UserSettings">
+          <Link className="link" to="UserSettings">
             <img
               className="icon"
-              src={process.env.PUBLIC_URL + "/assets/icons/settings.svg"}
+              src={process.env.PUBLIC_URL + '/assets/icons/settings.svg'}
               alt="Settings"
             />
             Settings
