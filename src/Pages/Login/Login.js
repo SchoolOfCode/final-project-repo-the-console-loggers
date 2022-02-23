cb04dff3678a1a8c71fa83439a6cc7f8d79a5915
Sevi <correosevi@gmail.com>
2022-02-23 16:33:19 +0000

    Add loading to the screen
    Co-authored-by: Victoriam97 <victoriameah@hotmail.com>
    Co-authored-by: JEveriss <jacques.everiss@gmail.com>

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,23 +1,23 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const navigate = useNavigate();
   useEffect(() => {
-   isAuthenticated && navigate('/home')
-  },);
-  const { loginWithRedirect, isAuthenticated} = useAuth0();
-  return  (
+    isAuthenticated && navigate('/home');
+  });
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  return !isLoading ? (
     <div className="login-wrapper">
       <img
         src={process.env.PUBLIC_URL + '/assets/logo.svg'}
         className="logo"
         alt="Logo"
       ></img>
-      <h1 className="name">Fridgeful</h1>
+      <h1 className="name">App name</h1>
       <h2 className="tag-line">
-      Buy wisely, cook Fridgefully, eat it all.
+        A recipe is a story that ends with a good meal..
       </h2>
       <button className="login-button" onClick={() => loginWithRedirect()}>
         Log In
@@ -26,7 +26,9 @@ function Login() {
         Don’t have an account? <span>Sign up</span>
       </div>
     </div>
-  ) 
+  ) : (
+    'Loading'
+  );
 }
 
 export default Login;
