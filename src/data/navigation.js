33408ed2b98cbaf4fcdb97cdfe18686b1bba9123
Sevi <correosevi@gmail.com>
2022-02-24 11:47:18 +0000

    Create data folder for navigation menu
    Co-authored-by: Victoriam97 <victoriameah@hotmail.com>
    Co-authored-by: JEveriss <jacques.everiss@gmail.com>

---
+++ src/data/navigation.js
@@ -0,0 +1,30 @@
+export const pathWay = [
+  {
+    id: 1,
+    path: '/Home',
+    name: 'Home',
+    activeImg: '/assets/icons/fridge-color.svg',
+    noActiveImg: '/assets/icons/fridge.svg',
+  },
+  {
+    id: 2,
+    path: '/RecipeList',
+    name: 'RecipeList',
+    activeImg: '/assets/icons/recipes-color.svg',
+    noActiveImg: '/assets/icons/recipes.svg',
+  },
+  {
+    id: 3,
+    path: '/ShoppingList',
+    name: 'ShoppingList',
+    activeImg: '/assets/icons/shopping-list-color.svg',
+    noActiveImg: '/assets/icons/shopping-list.svg',
+  },
+  {
+    id: 4,
+    path: '/UserSettings',
+    name: 'UserSettings',
+    activeImg: '/assets/icons/settings-color.svg',
+    noActiveImg: '/assets/icons/settings.svg',
+  },
+];
