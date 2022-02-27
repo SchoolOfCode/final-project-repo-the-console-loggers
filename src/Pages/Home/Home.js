4681daac5b4505445572045442d6e03484d39e11
21ciprian <2021ciprianoffice@gmail.com>
2022-02-27 19:22:20 +0000

    Added prettierrc.json file

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,60 +1,60 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import Card from '../../components/Card/Card';
-import GreenBanner from '../../components/GreenBanner/GreenBanner';
-import Button from '../../components/Ui/Button/Button';
-import Login from '../Login/Login';
-import { fridgeIngredients } from '../../data/navigation';
+import { useAuth0 } from '@auth0/auth0-react'
+import Card from '../../components/Card/Card'
+import GreenBanner from '../../components/GreenBanner/GreenBanner'
+import Button from '../../components/Ui/Button/Button'
+import { fridgeIngredients } from '../../data/navigation'
+import Login from '../Login/Login'
 
 function Home() {
-  const { isAuthenticated, isLoading } = useAuth0();
+    const { isAuthenticated, isLoading } = useAuth0()
 
-  //Date variables
-  let date = new Date();
-  let today = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    //Date variables
+    let date = new Date()
+    let today = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 
-  if (isLoading) {
-    return <h1>Loading</h1>;
-  }
-  return isAuthenticated ? (
-    <main className="main-home">
-      <GreenBanner text="+ ADD NEW ITEM" />
+    if (isLoading) {
+        return <h1>Loading</h1>
+    }
+    return isAuthenticated ? (
+        <main className='main-home'>
+            <GreenBanner text='+ ADD NEW ITEM' />
 
-      {fridgeIngredients.map((item) => {
-        return (
-          <Card
-            key={item.id}
-            name={item.name}
-            expdate={item.expiryDate}
-            quantity={item.quantity}
-          >
-            <span
-              className={`expiry-dot ${
-                today >= item.expiryDate ? 'red' : 'green'
-              }`}
-            ></span>
-          </Card>
-        );
-      })}
-      <div className="buttons-container-home">
-        <Button
-          text="Cook"
-          backgroundColor="transparent"
-          textColor="green"
-          width="halfLength"
-        />
-        <Button
-          text="Delete"
-          backgroundColor="red-button"
-          textColor="white"
-          width="halfLength"
-        />
-      </div>
-    </main>
-  ) : (
-    <div className="app">
-      <Login />
-    </div>
-  );
+            {fridgeIngredients.map((item) => {
+                return (
+                    <Card
+                        key={item.id}
+                        name={item.name}
+                        expdate={item.expiryDate}
+                        quantity={item.quantity}
+                    >
+                        <span
+                            className={`expiry-dot ${
+                                today >= item.expiryDate ? 'red' : 'green'
+                            }`}
+                        ></span>
+                    </Card>
+                )
+            })}
+            <div className='buttons-container-home'>
+                <Button
+                    text='Cook'
+                    backgroundColor='transparent'
+                    textColor='green'
+                    width='halfLength'
+                />
+                <Button
+                    text='Delete'
+                    backgroundColor='red-button'
+                    textColor='white'
+                    width='halfLength'
+                />
+            </div>
+        </main>
+    ) : (
+        <div className='app'>
+            <Login />
+        </div>
+    )
 }
 
-export default Home;
+export default Home
