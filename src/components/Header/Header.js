c85646e08d922de53a8f14ba5444e78a3b095cba
Sevi <correosevi@gmail.com>
2022-02-28 20:27:59 +0000

    Fix return warning on the welcomeMessages map
    Co-authored-by: Victoriam97 <victoriameah@hotmail.com>
    Co-authored by: Hyosssssun <hyosssssun@gmail.com>
    Co-authored-by: maximilianosinopoli <m_sinopoli@hotmail.com>
    Co-authored-by: 21ciprian <2021ciprianoffice@gmail.com>
    Co-authored-by: JEveriss <jacques.everiss@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,32 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
-import { welcomeMessages } from '../../data/navigation';
+import { welcomeMessages } from '../../data/navigation'
 
 const Header = ({ user, isAuthenticated, isLoading }) => {
-
-    let location = useLocation()
-      return (
-        <header className='header'>
-          {isLoading ? (
-            'loading'
-          ) : (
-            <>
-              <div className='avatar'>
-                <img src={user.picture} alt='Avatar' />
-              </div>
-              <div className='welcome-msg'>
-                <p className='welcome-name'>
-                  Welcome <span>{user.name}</span>
-                </p>
-                <p className='catch-phrase'>
-                {welcomeMessages.map((item)=>{
-                  if(location.pathname === item.path){ 
-                     return item.message }
-                    })}
-               </p>
-              </div>
-            </>
-          )}
-        </header>
-      )
-};
-
+  let location = useLocation()
+  return (
+    <header className='header'>
+      {isLoading ? (
+        'loading'
+      ) : (
+        <>
+          <div className='avatar'>
+            <img src={user.picture} alt='Avatar' />
+          </div>
+          <div className='welcome-msg'>
+            <p className='welcome-name'>
+              Welcome <span>{user.name}</span>
+            </p>
+            <p className='catch-phrase'>
+              {welcomeMessages.map(
+                (item) => location.pathname === item.path && item.message
+              )}
+            </p>
+          </div>
+        </>
+      )}
+    </header>
+  )
+}
 
 export default Header
