4f8a6e1562e0272e384b58142ffcc97ca0b0da24
Jacques Everiss <jacques.everiss@gmail.com>
2022-02-28 15:39:21 +0000

    added scroll to top Co-authored-by: Victoriam97 <victoriameah@hotmail.com> Co-authored by: Hyosssssun <hyosssssun@gmail.com> Co-authored-by: maximilianosinopoli <m_sinopoli@hotmail.com> Co-authored-by: 21ciprian <2021ciprianoffice@gmail.com> Co-authored-by: ajsevillano<correosevi@gmail.com>

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,23 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "../Pages/Home/Home";
-import RecipeList from "../Pages/RecipeList/RecipeList";
-import ShoppingList from "../Pages/ShoppingList/ShoppingList";
-import UserSettings from "../Pages/UserSettings/UserSettings";
-import AddIngredient from "../Pages/AddIngredient/AddIngredient";
-import Login from "../Pages/Login/Login";
-import FullRecipe from "../Pages/FullRecipe/FullRecipe";
+import { Routes, Route } from 'react-router-dom'
+import Home from '../Pages/Home/Home'
+import RecipeList from '../Pages/RecipeList/RecipeList'
+import ShoppingList from '../Pages/ShoppingList/ShoppingList'
+import UserSettings from '../Pages/UserSettings/UserSettings'
+import AddIngredient from '../Pages/AddIngredient/AddIngredient'
+import Login from '../Pages/Login/Login'
+import FullRecipe from '../Pages/FullRecipe/FullRecipe'
 
 function Router() {
   return (
     <Routes>
-      <Route path="home" element={<Home />} />
-      <Route path="/" element={<Login />} />
-      <Route path="RecipeList" element={<RecipeList />} />
-      <Route path="ShoppingList" element={<ShoppingList />} />
-      <Route path="UserSettings" element={<UserSettings />} />
-      <Route path="FullRecipe" element={<FullRecipe />} />
-      <Route path="Home/AddIngredient" element={<AddIngredient />} />
+      <Route path='home' element={<Home />} />
+      <Route path='/' element={<Login />} />
+      <Route path='RecipeList' element={<RecipeList />} />
+      <Route path='ShoppingList' element={<ShoppingList />} />
+      <Route path='UserSettings' element={<UserSettings />} />
+      <Route path='FullRecipe' element={<FullRecipe />} />
+      <Route path='Home/AddIngredient' element={<AddIngredient />} />
     </Routes>
-  );
+  )
 }
-export default Router;
+export default Router
