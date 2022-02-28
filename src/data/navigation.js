f8003dedd0a873b6d2bc963654399ba78ae5d718
Hyosssssun <hyosssssun@gmail.com>
2022-02-28 16:09:49 +0000

    welcome message added
    Co-authored-by: ajsevillano<correosevi@gmail.com>
    Co-authored-by: Victoriam97 <victoriameah@hotmail.com>
    Co-authored-by: JEveriss <jacques.everiss@gmail.com>
    Co-authored-by: Maximilianosinopoli <m_sinopoli@hotmail.com>
    Co-authored-by: 21ciprian <2021ciprianoffice@gmail.com>

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -43,3 +43,21 @@ export const addFoodType = [
   { food: "Tofu" },
 ];
 
+export const welcomeMessages = [
+  { id: 1, path: '/', message: 'welcome' },
+  { id: 2, path: '/Home', message: 'What ingredient shall we use today?' },
+  {
+    id: 3,
+    path: '/Home/AddIngredient',
+    message: 'Add what you have in your fridge!',
+  },
+  { id: 4, path: '/RecipeList', message: "Let's cook" },
+  { id: 5, path: '/FullRecipe', message: 'Yummyyy' },
+  { id: 6, path: '/ShoppingList', message: 'Buy wisely, do not waste it' },
+  {
+    id: 7,
+    path: '/ShoppingList/AddItem',
+    message: 'Add what you need to buy',
+  },
+  { id: 8, path: '/UserSettings', message: 'Change me how you like!' },
+]
