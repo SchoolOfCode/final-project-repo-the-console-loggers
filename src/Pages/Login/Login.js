c4d561b0508ec4ebc2b11ef59114100ad134c2e5
Sevi <correosevi@gmail.com>
2022-03-03 15:02:32 +0000

    Add signup link on login screen
    Co-authored-by: Victoriam97 <victoriameah@hotmail.com>
    Co-authored by: Hyosssssun <hyosssssun@gmail.com>
    Co-authored-by: maximilianosinopoli <m_sinopoli@hotmail.com>
    Co-authored-by: 21ciprian <2021ciprianoffice@gmail.com>
    Co-authored-by: JEveriss <jacques.everiss@gmail.com>

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,37 +1,35 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-    const navigate = useNavigate()
-    useEffect(() => {
-        isAuthenticated && navigate('/Home')
-    })
-    const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0()
-    return !isLoading ? (
-        <div className='login-wrapper'>
-            <img
-                src={process.env.PUBLIC_URL + '/assets/logo.svg'}
-                className='logo'
-                alt='Logo'
-            ></img>
-            <h1 className='name'>App name</h1>
-            <h2 className='tag-line'>
-                A recipe is a story that ends with a good meal..
-            </h2>
-            <button
-                className='login-button'
-                onClick={() => loginWithRedirect()}
-            >
-                Log In
-            </button>
-            <div className='signUp'>
-                Don’t have an account? <span>Sign up</span>
-            </div>
-        </div>
-    ) : (
-        'Loading'
-    )
+  const navigate = useNavigate();
+  useEffect(() => {
+    isAuthenticated && navigate('/Home');
+  });
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  return !isLoading ? (
+    <div className='login-wrapper'>
+      <img
+        src={process.env.PUBLIC_URL + '/assets/logo.svg'}
+        className='logo'
+        alt='Logo'
+      ></img>
+      <h1 className='name'>Fridgeful</h1>
+      <h2 className='tag-line'>Buy wisely, cook Fridgefully, eat it all</h2>
+      <button className='login-button' onClick={() => loginWithRedirect()}>
+        Log In
+      </button>
+      <div className='signUp'>
+        Don’t have an account?{' '}
+        <span onClick={() => loginWithRedirect({ screen_hint: 'signup' })}>
+          Sign up
+        </span>
+      </div>
+    </div>
+  ) : (
+    'Loading'
+  );
 }
 
-export default Login
+export default Login;
