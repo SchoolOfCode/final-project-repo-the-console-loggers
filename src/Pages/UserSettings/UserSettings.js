d5935ae4df7f3d6cbd6e16a7e873d7ad35b1970e
Hyosssssun <hyosssssun@gmail.com>
2022-03-07 11:39:02 +0000

    delete button confirmation added
    Co-authored-by: ajsevillano<correosevi@gmail.com>
    Co-authored-by: Victoriam97 <victoriameah@hotmail.com>
    Co-authored-by: JEveriss <jacques.everiss@gmail.com>
    Co-authored-by: Maximilianosinopoli <m_sinopoli@hotmail.com>
    Co-authored-by: 21ciprian <2021ciprianoffice@gmail.com>

--- src/Pages/UserSettings/UserSettings.js
+++ src/Pages/UserSettings/UserSettings.js
@@ -1,16 +1,42 @@
+import React, { useState } from 'react'
 //Utils
 import { useAuth0 } from '@auth0/auth0-react';
 //Components
 import Card from '../../components/Card/Card';
 import Button from '../../components/Ui/Button/Button';
 import UserSettingsCard from '../../components/UserSettingsCard/UserSettingsCard';
+import Modal from '../../components/Modal/Modal';
 //Pages
 import Login from '../Login/Login';
 
 function UserSettings() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { isAuthenticated, logout } = useAuth0();
+  
+  function deleteAccount(){
+    console.log("i am deleted")
+  }
   return isAuthenticated ? (
     <main className='main-settings'>
+      {isModalOpen && (
+        <Modal isModalOpen={isModalOpen}>
+          <h1>Are you sure you want to delete your account?</h1>
+          <div className='button-container'>
+            <Button
+              text='Yes, I&#39;m sure'
+              backgroundColor='red-button'
+              textColor='white'
+              handleClick={deleteAccount}
+            />
+            <Button
+              text='Cancel'
+              backgroundColor='transparent'
+              textColor='green'
+              handleClick={() => setIsModalOpen(false)}
+            />
+          </div>
+        </Modal>
+      )}
       <h1 className='settings-title'>Settings</h1>
 
       <Card name='darkmode-lightmode' />
@@ -33,6 +59,7 @@ function UserSettings() {
         <h1 className='settings-card-title'>Delete account</h1>
         <div className='buttons-container-settings'>
           <Button
+            handleClick={() => setIsModalOpen(true)}
             text='Delete account'
             backgroundColor='red-button'
             textColor='white'
