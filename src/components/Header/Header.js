d475bfd3c89982b6821dd340393d80bf2dc71216
Sevi <correosevi@gmail.com>
2022-03-11 15:11:20 +0000

    Refactory loading
    Co-authored-by: Victoriam97 <victoriameah@hotmail.com>
    Co-authored by: Hyosssssun <hyosssssun@gmail.com>
    Co-authored-by: maximilianosinopoli <m_sinopoli@hotmail.com>
    Co-authored-by: 21ciprian <2021ciprianoffice@gmail.com>
    Co-authored-by: JEveriss <jacques.everiss@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,28 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
-import { welcomeMessages } from '../../data/navigation'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { welcomeMessages } from '../../data/navigation';
 
-const Header = ({ user, isAuthenticated, isLoading }) => {
-  let location = useLocation()
+const Header = ({ user }) => {
+  let location = useLocation();
   return (
     <header className='header'>
-      {isLoading ? (
-        'loading'
-      ) : (
-        <>
-          <div className='avatar'>
-            <img src={user.picture} alt='Avatar' />
-          </div>
-          <div className='welcome-msg'>
-            <p className='welcome-name'>
-              Welcome <span>{user.name}</span>
-            </p>
-            <p className='catch-phrase'>
-              {welcomeMessages.map(
-                (item) => location.pathname === item.path && item.message
-              )}
-            </p>
-          </div>
-        </>
-      )}
+      <>
+        <div className='avatar'>
+          <img src={user.picture} alt='Avatar' />
+        </div>
+        <div className='welcome-msg'>
+          <p className='welcome-name'>
+            Welcome <span>{user.name}</span>
+          </p>
+          <p className='catch-phrase'>
+            {welcomeMessages.map(
+              (item) => location.pathname === item.path && item.message
+            )}
+          </p>
+        </div>
+      </>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
